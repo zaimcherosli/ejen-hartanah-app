b01357c7004d1584da2ddc1183028bde491fafc0
zaimcherosli <huzaimrosli@gmail.com>
2026-08-07 12:54:13 +0800

    Bump system version to v1.7.2 (Fix Supabase schema handling for youtube_url)

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 
 // System Versioning & Build Info
 window.CEM_SYSTEM = {
-  VERSION: 'v1.7.1',
-  BUILD_DATE: '2026-08-05',
+  VERSION: 'v1.7.2',
+  BUILD_DATE: '2026-08-07',
   ENV: 'Production (Cloudflare Pages + R2 Storage)',
   PWA_VERSION: 'cem-pwa-v3'
 };
